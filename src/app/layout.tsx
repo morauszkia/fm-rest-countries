5527afc1dfc7fc5342c69e1ee9ff1e6c8654f69c
mantis <morauszki.andras@gmail.com>
2025-11-07 23:11:17 +0100

    Theme switching using next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header/Header";
+import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
   title: "Where in the world?",
@@ -19,10 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={nunitoSans.className}>
+    <html lang="en" className={nunitoSans.className} suppressHydrationWarning>
       <body>
-        <Header />
-        {children}
+        <ThemeProvider>
+          <Header />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/Header/ThemeToggle/ThemeToggle.tsx
+++ src/components/Header/ThemeToggle/ThemeToggle.tsx
@@ -1,12 +1,25 @@
+"use client";
+
+import { useTheme } from "next-themes";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun } from "@fortawesome/free-solid-svg-icons";
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
 import classes from "./ThemeToggle.module.css";
 
 export const ThemeToggle = () => {
+  const { theme, setTheme } = useTheme();
+
+  const toggleThemeHandler = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
-    <button className={classes.button}>
-      <FontAwesomeIcon icon={faSun} className={classes.icon} />
-      <span>Light Mode</span>
+    <button className={classes.button} onClick={toggleThemeHandler}>
+      <FontAwesomeIcon
+        icon={theme === "light" ? faMoon : faSun}
+        className={classes.icon}
+      />
+      <span>{theme === "light" ? "Dark" : "Light"} mode</span>
     </button>
   );
 };
